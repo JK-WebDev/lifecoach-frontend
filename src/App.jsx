b22be5633e5efdc0257edcbd6c77295c9329b6bd
Kyle Cordell <70975298+kcords@users.noreply.github.com>
2023-08-31 15:11:50 -0700

    Adds login functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,31 @@
+/* eslint-disable react-refresh/only-export-components */
 import { Component } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { withAuth0 } from "@auth0/auth0-react";
 
-import { Header, Welcome, Footer } from "./components/";
+import { Welcome, Home, About } from "./pages/";
+import { Header, Footer } from "./components/";
 
-export default class App extends Component {
-  render() {
-    return (
-      <Router>
-        <Header />
-        <main className="p-3">
-          <Routes>
-            <Route exact path="/" element={<Welcome />} />
-          </Routes>
-        </main>
-        <Footer />
-      </Router>
-    );
+export default withAuth0(
+  class App extends Component {
+    render() {
+      const { isAuthenticated } = this.props.auth0;
+      return (
+        <Router>
+          <Header />
+          <main className="p-3">
+            <Routes>
+              <Route
+                exact
+                path="/"
+                element={isAuthenticated ? <Home /> : <Welcome />}
+              />
+              <Route exact path="/about" element={<About />} />
+            </Routes>
+          </main>
+          <Footer />
+        </Router>
+      );
+    }
   }
-}
+);
