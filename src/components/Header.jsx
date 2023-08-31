346ce2403ba0b3353798b574327fd85241dafd10
Kyle Cordell <70975298+kcords@users.noreply.github.com>
2023-08-31 14:14:30 -0700

    Adds initial navigation options

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import { Component } from "react";
 
+import { Link } from "react-router-dom";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
@@ -15,13 +16,21 @@ export default class Header extends Component {
     };
 
     return (
-      <header className="bg-secondary bg-gradient fixed-top">
+      <header className="bg-secondary bg-gradient sticky-top">
         <Navbar>
           <Container fluid>
-            <Nav>
+            <Nav variant="underline">
               <Navbar.Brand>{strings.brand}</Navbar.Brand>
-              <Nav.Link>{strings.home}</Nav.Link>
-              <Nav.Link>{strings.about}</Nav.Link>
+              <Nav.Item>
+                <Nav.Link as={Link} to="/">
+                  {strings.home}
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link as={Link} to="/about">
+                  {strings.about}
+                </Nav.Link>
+              </Nav.Item>
             </Nav>
             <Nav>
               <LoginButton />
