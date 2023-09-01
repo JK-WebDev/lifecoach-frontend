54b5ecbf858d88d456c94864ef36339b72e643d6
Josh Easley <easleyjs@gmail.com>
2023-09-01 08:16:59 -0700

    Updates styling on TeamSidebar component

--- src/components/TeamSidebar.jsx
+++ src/components/TeamSidebar.jsx
@@ -7,16 +7,17 @@ class TeamSidebar extends React.Component {
         const { teamData, selectedTm, setSelectedTm } = this.props;
 
         return (
-            <ListGroup>{ Object.keys(teamData).map((tmKey, idx) => {
+            <ListGroup>
+                { Object.keys(teamData).map((tmKey, idx) => {
                     const { name } = teamData[tmKey];
 
                     return <ListGroup.Item key={ idx } 
-                                           onClick={ () => { setSelectedTm(tmKey) } }
+                                           onClick={ () => { setSelectedTm(tmKey) }}
                                            active={ tmKey == selectedTm ?? 'true' }
                                            style={{ cursor: 'pointer' }}>
                                 { name }
                            </ListGroup.Item>
-                })}
+                })}                
             </ListGroup>
         )
     }
