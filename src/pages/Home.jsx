69d49fd868a49025f01a1aac434dda7da049a984
Kyle Cordell <70975298+kcords@users.noreply.github.com>
2023-09-01 14:14:31 -0700

    Adds PromptInput and ResponseCard to Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,33 @@
 import { Component } from "react";
 
+import Container from "react-bootstrap/Container";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+
+import { PromptInput, ResponseCard } from "../components";
+
 export default class Home extends Component {
   render() {
+    const strings = {
+      instructionText:
+        "Enter your question below to receive an actionable task from Uncle Jimmy",
+    };
+
     return (
       <>
-        <p>Welcome home!</p>
+        <Container className="text-center container-md">
+          <Col>
+            <Row>
+              <h1 className="fs-5">{strings.instructionText}</h1>
+            </Row>
+            <Row className="my-3">
+              <PromptInput />
+            </Row>
+            <Row>
+              <ResponseCard />
+            </Row>
+          </Col>
+        </Container>
       </>
     );
   }
