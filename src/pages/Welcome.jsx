c2e446fbe28042bd76ea4c0e94fd63401988341d
Kyle Cordell <70975298+kcords@users.noreply.github.com>
2023-09-02 16:23:45 -0700

    Updates Welcome page message

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -3,7 +3,8 @@ import { Component } from "react";
 export default class Welcome extends Component {
   render() {
     const strings = {
-      welcomeMsg: "Hey there! Please sign in to get started.",
+      welcomeMsg:
+        "Hey there! Feel free to sign in if you're ready to shape up and get your life in order.",
     };
     return <p>{strings.welcomeMsg}</p>;
   }
