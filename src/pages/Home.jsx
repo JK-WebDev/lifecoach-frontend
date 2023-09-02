15356942a3b310074bc442f4e24be64da8865f9a
Kyle Cordell <70975298+kcords@users.noreply.github.com>
2023-09-01 20:04:22 -0700

    Adds query response from API

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,7 @@
+/* eslint-disable react-refresh/only-export-components */
 import { Component } from "react";
+import { withAuth0 } from "@auth0/auth0-react";
+import axios from "axios";
 
 import Container from "react-bootstrap/Container";
 import Col from "react-bootstrap/Col";
@@ -6,29 +9,74 @@ import Row from "react-bootstrap/Row";
 
 import { PromptInput, ResponseCard } from "../components";
 
-export default class Home extends Component {
-  render() {
-    const strings = {
-      instructionText:
-        "Enter your question below to receive an actionable task from Uncle Jimmy",
+export default withAuth0(
+  class Home extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        generatedResponse: null,
+      };
+    }
+
+    getToken = () => {
+      return this.props.auth0
+        .getIdTokenClaims()
+        .then((res) => res.__raw)
+        .catch((err) => console.error(err));
     };
 
-    return (
-      <>
-        <Container className="text-center container-md">
-          <Col>
-            <Row>
-              <h1 className="fs-5">{strings.instructionText}</h1>
-            </Row>
-            <Row className="my-3">
-              <PromptInput />
-            </Row>
-            <Row>
-              <ResponseCard />
-            </Row>
-          </Col>
-        </Container>
-      </>
-    );
+    getGeneratedTask = async (query) => {
+      const route = "/query";
+      const url = `${import.meta.env.VITE_SERVER_URL}${route}`;
+      const jwt = await this.getToken();
+      const config = {
+        headers: { Authorization: `Bearer ${jwt}` },
+      };
+      axios
+        .post(url, { query }, config)
+        .then(({ data }) => this.updateGeneratedResponse(data))
+        //TODO Implement error handling
+        .catch((err) => console.error(err));
+    };
+
+    updateGeneratedResponse = (generatedResponse = null) => {
+      this.setState({ generatedResponse });
+    };
+
+    render() {
+      const strings = {
+        instructionText:
+          "Enter your question below to receive an actionable task from Uncle Jimmy",
+      };
+
+      const {
+        getGeneratedTask,
+        updateGeneratedResponse,
+        state: { generatedResponse },
+      } = this;
+
+      return (
+        <>
+          <Container className="text-center container-md">
+            <Col>
+              <Row>
+                <h1 className="fs-5">{strings.instructionText}</h1>
+              </Row>
+              <Row className="my-3">
+                <PromptInput getGeneratedTask={getGeneratedTask} />
+              </Row>
+              {generatedResponse && (
+                <Row>
+                  <ResponseCard
+                    generatedResponse={generatedResponse}
+                    updateGeneratedResponse={updateGeneratedResponse}
+                  />
+                </Row>
+              )}
+            </Col>
+          </Container>
+        </>
+      );
+    }
   }
-}
+);
