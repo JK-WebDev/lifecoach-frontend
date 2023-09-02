36f1f59a8720a68e7c7df91c240479464c8759ad
Kyle Cordell <70975298+kcords@users.noreply.github.com>
2023-09-02 11:09:53 -0700

    Update index.jsx

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,8 +1,5 @@
 //SITE EXPORTS
 export { default as Header } from "./Header";
-export { default as PromptInput } from "./PromptInput";
-export { default as ResponseCard } from "./ResponseCard";
-export { default as TaskList } from "./TaskList";
 export { default as Footer } from "./Footer";
 export { default as ToastMessage } from "./ToastMessage";
 
