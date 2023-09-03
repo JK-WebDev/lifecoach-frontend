ec7513bf2fa72a23d45d206943f5bfbb1f57c95e
Kyle Cordell <70975298+kcords@users.noreply.github.com>
2023-09-02 22:51:04 -0700

    Corrects api bug

--- src/api/axios.js
+++ src/api/axios.js
@@ -4,11 +4,11 @@ import axios from "axios";
 
 // methods are async, but are not awaited, so they will need to be awaited at call or chained with .then to handle the promise
 
-const REQ_TIMEOUT = 10000;
+const REQ_TIMEOUT = 15000;
 
 const api = {};
 
-api.generateConfig = (token) => ({
+const generateConfig = (token) => ({
   headers: { Authorization: `Bearer ${token}` },
   timeout: REQ_TIMEOUT,
 });
@@ -16,35 +16,35 @@ api.generateConfig = (token) => ({
 api.queryPost = async (query, token) => {
   const route = "/query";
   const url = `${import.meta.env.VITE_SERVER_URL}${route}`;
-  const config = this.generateConfig(token);
+  const config = generateConfig(token);
   return axios.post(url, { query }, config);
 };
 
 api.taskGet = async (token) => {
   const route = "/task";
   const url = `${import.meta.env.VITE_SERVER_URL}${route}`;
-  const config = this.generateConfig(token);
+  const config = generateConfig(token);
   return axios.get(url, config);
 };
 
 api.taskPost = async (task, token) => {
   const route = "/task";
   const url = `${import.meta.env.VITE_SERVER_URL}${route}`;
-  const config = this.generateConfig(token);
+  const config = generateConfig(token);
   return axios.post(url, task, config);
 };
 
 api.taskPatch = async (id, task, token) => {
   const route = `/task/${id}`;
   const url = `${import.meta.env.VITE_SERVER_URL}${route}`;
-  const config = this.generateConfig(token);
+  const config = generateConfig(token);
   return axios.patch(url, task, config);
 };
 
 api.taskDelete = async (id, token) => {
   const route = `/task/${id}`;
   const url = `${import.meta.env.VITE_SERVER_URL}${route}`;
-  const config = this.generateConfig(token);
+  const config = generateConfig(token);
   axios.delete(url, config);
 };
 
