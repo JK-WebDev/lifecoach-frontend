e4d321966ae9aaab99a8a20af8f6b0dc12c73b9a
Kyle Cordell <70975298+kcords@users.noreply.github.com>
2023-09-04 12:57:51 -0700

    Adds created timestamp to TaskListItem

--- src/components/home/TaskListItem.jsx
+++ src/components/home/TaskListItem.jsx
@@ -1,12 +1,14 @@
 import { Component } from "react";
-
 import "./TaskListItem.css";
+import Time from "../../utilities/time";
 
 import Card from "react-bootstrap/Card";
 
 export default class TaskListItem extends Component {
   render() {
     const { task, setSelectedTask } = this.props;
+    const createdAtTimeAgo = Time.getTimeAgo(task.createdAt);
+    console.log({ createdAtTimeAgo });
 
     return (
       <Card
@@ -16,11 +18,17 @@ export default class TaskListItem extends Component {
       >
         <Card.Body>
           <Card.Text
-            className={`${
+            className={`d-flex flex-column justify-content-end ${
               task.isCompleted ? "text-decoration-line-through" : ""
             }`}
           >
             {task.title}
+            <p
+              className="mt-1 mb-0 ms-auto text-black-50"
+              style={{ fontSize: ".75rem" }}
+            >
+              {`Added ${createdAtTimeAgo}`}
+            </p>
           </Card.Text>
         </Card.Body>
       </Card>
