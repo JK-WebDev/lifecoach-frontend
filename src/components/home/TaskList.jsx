5af6e1d6cfd793fb841e67dd24fc9447860152fc
Kyle Cordell <70975298+kcords@users.noreply.github.com>
2023-09-03 18:35:19 -0700

    Cleans up no tasks message display

--- src/components/home/TaskList.jsx
+++ src/components/home/TaskList.jsx
@@ -2,6 +2,7 @@ import { Component } from "react";
 
 import Button from "react-bootstrap/Button";
 import Offcanvas from "react-bootstrap/Offcanvas";
+import Card from "react-bootstrap/Card";
 import Spinner from "react-bootstrap/Spinner";
 
 import TaskListItem from "./TaskListItem";
@@ -29,9 +30,7 @@ export default class TaskList extends Component {
     const strings = {
       buttonLabel: "View my tasks",
       listHeader: "My Life Improvement Tasks",
-      noTasksLabel: "No Tasks! \u{1F633}",
-      noTasksMsg:
-        "Oh, so you think you're too good for my life-changing advice? \u{1F974} Alright, fine! I guess you're just floating through life without a care in the world. \u{1F913} Good for you! Let me know when you're ready to get started with some tasks.",
+      noTasksMsg: `No Tasks! \u{1F633}\n\nOh, so you think you're too good for my life-changing advice? \u{1F974}\n\nAlright, fine! I guess you're just floating through life without a care in the world. \u{1F913} Good for you!\n\nLet me know when you're ready to get started with some tasks.`,
     };
 
     return (
@@ -72,10 +71,14 @@ export default class TaskList extends Component {
                 />
               ))
             ) : (
-              <>
-                <h3 className="fs-6 my-4">{strings.noTasksLabel}</h3>
-                <p>{strings.noTasksMsg}</p>
-              </>
+              <Card
+                body
+                border="danger"
+                style={{ cursor: "pointer" }}
+                onClick={toggleListVisibile}
+              >
+                <p style={{ whiteSpace: "pre-wrap" }}>{strings.noTasksMsg}</p>
+              </Card>
             )}
           </Offcanvas.Body>
         </Offcanvas>
