787645ae929ba947e826fabd5b5749c9611ea92b
Kyle Cordell <70975298+kcords@users.noreply.github.com>
2023-09-04 14:55:36 -0700

    Adds notes label to TaskListItem

--- src/components/home/TaskListItem.jsx
+++ src/components/home/TaskListItem.jsx
@@ -7,8 +7,10 @@ import Card from "react-bootstrap/Card";
 export default class TaskListItem extends Component {
   render() {
     const { task, setSelectedTask } = this.props;
+    const strings = {
+      notesText: "notes",
+    };
     const createdAtTimeAgo = Time.getTimeAgo(task.createdAt);
-    console.log({ createdAtTimeAgo });
 
     return (
       <Card
@@ -17,18 +19,30 @@ export default class TaskListItem extends Component {
         onClick={() => setSelectedTask(task)}
       >
         <Card.Body>
-          <Card.Text
-            className={`d-flex flex-column justify-content-end ${
-              task.isCompleted ? "text-decoration-line-through" : ""
-            }`}
-          >
-            {task.title}
+          <Card.Text as={"div"}>
             <p
-              className="mt-1 mb-0 ms-auto text-black-50"
-              style={{ fontSize: ".75rem" }}
+              className={`mb-0 ${
+                task.isCompleted ? "text-decoration-line-through" : ""
+              }`}
             >
-              {`Added ${createdAtTimeAgo}`}
+              {task.title}
             </p>
+
+            <div className="d-flex justify-content-between">
+              <p
+                className="mt-1 mb-0 me-auto text-black-50"
+                style={{ fontSize: ".75rem" }}
+              >
+                {task.notes?.length > 0 &&
+                  `${task.notes.length} ${strings.notesText}`}
+              </p>
+              <p
+                className="mt-1 mb-0 ms-auto text-black-50"
+                style={{ fontSize: ".75rem" }}
+              >
+                {`Added ${createdAtTimeAgo}`}
+              </p>
+            </div>
           </Card.Text>
         </Card.Body>
       </Card>
